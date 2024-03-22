132e9efeab1a7b5b505c15b98a31fcb24a2c41bd
shahed137003 <ahmedmohamed.aa777@gmail.com>
2024-03-22 16:13:33 +0200

    we had a modification on the lad1 solutions

--- sheets solution/sheet 1 solutions/lab1.java
+++ sheets solution/sheet 1 solutions/lab1.java
@@ -1,4 +1,22 @@
+public class Mavenproject1 {
+static double area(double length,double width){
+return length*width;
+}
+static double area(double rad){
+return Math.PI*rad*rad;
+}
+static int sumArr(int arr[]){
+    int sum=0;
+for(int i=0;i<arr.length;i++)
+sum+=arr[i];
+return sum;
+}
 
+    public static void main(String[] args) {
+      System.out.println("the first question");
+double areaRea=area(4,5.5);
+ double areaCir=area(5);   
+System.out.println("the area of rectangle = "+areaRea+" and the area of circle = "+areaCir);
 System.out.println("the second question");
 for(int i=0;i<10;i++){ 
  if (i%2 == 0) 
@@ -19,4 +37,6 @@ break; //nothing will be printed
 
    }
    System.out.println("");
-   }  
+   }
+    }
+}
